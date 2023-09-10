fb49db96d0952acef89ffa3121b8126716d1a073
Shamil Garipov <zlonamerenniy@yandex.ru>
2023-09-10 18:09:54 +0500

    форма оранжевая и длинная

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,8 +45,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Yellow;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(1286, 421);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "БЛОКНОТ";
