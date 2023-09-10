577b645556eedc78f8a0156154e441fb9ed28550
Shamil Garipov <zlonamerenniy@yandex.ru>
2023-09-10 18:11:08 +0500

    зеленая форма и нет кнопки

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,26 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(288, 188);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 46);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Закрыть";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(1286, 421);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "БЛОКНОТ";
             this.ResumeLayout(false);
@@ -55,8 +43,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
